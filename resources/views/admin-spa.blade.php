0a5a91e52a920297b3d796111e4956ebb6f3236c
smirnoff <smirnoffnew@gmail.com>
2019-03-02 12:15:29 +0200

    progress structure

--- resources/views/admin-spa.blade.php
+++ resources/views/admin-spa.blade.php
@@ -1,21 +1,17 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <script>window.Laravel= <?= json_encode(['csrfToken' => csrf_token()]); ?></script>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
-        <link href="{{ URL::asset('/css/admin-app.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/admin-vue.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/admin-global-app.css') }}" rel="stylesheet" type="text/css">
         <title>ADMIN SPA</title>
     </head>
 
     <body>
-        <div id="app">
-            {!! $ssr !!}
-        </div>
-        <script src="{{ URL::asset('js/admin-entry-client.js') }}" type="text/javascript"></script>
+        {!! $ssr !!}
+        <script src="{{ asset('js/admin-entry-client.js') }}" type="text/javascript"></script>
     </body>
 </html>
 
--- resources/views/client-spa.blade.php
+++ resources/views/client-spa.blade.php
@@ -1,15 +1,16 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('/css/client-vue.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('/css/client-global-app.css') }}" rel="stylesheet" type="text/css">
-    <title>Vue/Laravel SSR Multi-Page App</title>
-</head>
-<body>
-    {!! $ssr !!}
-    <script src="{{ asset('js/client-entry-client.js') }}" type="text/javascript"></script>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui">
+        <link href="{{ asset('/css/client-vue.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/client-global-app.css') }}" rel="stylesheet" type="text/css">
+        <title>CLIENT SPA</title>
+    </head>
+
+    <body>
+        {!! $ssr !!}
+        <script src="{{ asset('js/client-entry-client.js') }}" type="text/javascript"></script>
+    </body>
 </html>
